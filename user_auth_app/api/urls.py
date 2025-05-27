52b871d9c3e113b66f6a55b33bbc2142111ac4d8
Dominik Marnet <oxgameboyxo@googlemail.com>
2025-05-27 15:28:33 +0200

    added: global path 'api/auth/', local paths 'registration/' + 'login/', UserProfile model

---
+++ user_auth_app/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import RegistrationView
+from rest_framework.authtoken.views import obtain_auth_token
+
+
+
+urlpatterns = [
+    path('registration/', RegistrationView.as_view(), name='registration'),
+    path('login/', obtain_auth_token, name='login'),
+]
