4dec461f3dbdec865f9684c4510d1e353a6f637e
Dominik Marnet <oxgameboyxo@googlemail.com>
2025-05-28 15:04:54 +0200

    added:Comment Model

--- forum_app/models.py
+++ forum_app/models.py
@@ -13,4 +13,10 @@ class Post(models.Model):
         return self.title
 
 class Comment(models.Model):
-    pass
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255, default='No comment text yet!')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at =  models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.post
