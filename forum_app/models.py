f408e96b00961a47581d1a740ae81720db39bbe9
Dominik Marnet <oxgameboyxo@googlemail.com>
2025-05-30 08:56:50 +0200

    refactor: Post model

--- forum_app/models.py
+++ forum_app/models.py
@@ -6,11 +6,11 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=55, default='No title yet!')
     content = models.CharField(max_length=255, default='No content yet!')
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at =  models.DateField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
+    created_at =  models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return f"post {self.title} from {self.author}"
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
